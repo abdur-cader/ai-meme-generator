8dbb62a3fd6f88cdb772bd49c24430b1df1c5b6c
panko <bagu.2k5.58@gmail.com>
2025-02-23 09:34:09 +0400

    Update gemini_main.py

--- gemini_tuned/gemini_main.py
+++ gemini_tuned/gemini_main.py
@@ -7,7 +7,7 @@ genai.configure(api_key="AIzaSyAZQOELgFxmhqaonFel5j4BWbvsCgh2Kwo")
 models = list(genai.list_tuned_models())
 config = GenerationConfig(max_output_tokens = 10)
 
-model = genai.GenerativeModel(model_name="tunedModels/memecaptionerv1-am02zm7iaov6")
+model = genai.GenerativeModel(model_name="tunedModels/PASTE-MODEL-NAME-HERE")
 
 def gemini_generate(intext):
     result = model.generate_content(f"generate meme caption: {intext}", generation_config=config)
